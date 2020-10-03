229204c4e72d072249806aef92d6504ac6d3ea77
bob.nem <bob.nem@ya.ru>
2020-10-03 23:09:44 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="http://animateyourlogo.net/wp-content/uploads/2018/06/sample-logo-design-freepik.png" />
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://photocentra.ru/images/main87/870175_main.jpg' />
+      </div>
+      <div>
+        avatar with description
+      </div>
+      <div>
+        my posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Profile;
